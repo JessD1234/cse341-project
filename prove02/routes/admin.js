3e6f0156ce58c3debb5aa219e1e448962539210e
JessD1234 <46542907+JessD1234@users.noreply.github.com>
2021-09-21 19:35:28 -0600

    Finished readings

--- prove02/routes/admin.js
+++ prove02/routes/admin.js
@@ -5,18 +5,37 @@ const path = require('path');
 
 const router = express.Router();
 
+const products = [];
+const users = [];
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(routeFinder, 'views', 'add-product.html'));
+    //res.sendFile(path.join(routeFinder, 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
 router.post('/add-product', (req, res, next) => {
+    products.push({title: req.body.title});
     console.log(req.body);
+    console.log(products);
+    console.log(products.title);
     res.redirect('/');
 });
 
-router.use('/users', (req, res, next) => {
-    console.log('Users middleware');
-    res.send('<h1>Users middleware</h1>');
+router.get('/add-users', (req, res, next) => {
+    res.render('add-users', {pageTitle: 'Add User', path: '/admin/add-users'})
+   //res.sendFile(path.join(routeFinder, 'views', 'users.html'));
 });
 
-module.exports = router;
+router.post('/add-users', (req, res, next) => {
+    users.push({name: req.body.name});
+  console.log(users);
+    res.redirect('/admin/display-users');
+});
+
+router.get('/display-users', (req, res, next) => {
+    res.render('display-users', {users: users, pageTitle: 'Users', path: '/admin/display-users'})
+   //res.sendFile(path.join(routeFinder, 'views', 'users.html'));
+});
+
+exports.routes = router;
+exports.products = products;
--- prove02/routes/shop.js
+++ prove02/routes/shop.js
@@ -3,11 +3,21 @@ const express = require('express');
 const path = require('path');
 
 const routeFinder = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(routeFinder, 'views', 'shop.html'));
+  const products = adminData.products;
+  console.log(products);
+   // res.sendFile(path.join(routeFinder, 'views', 'shop.html'));
+   res.render('shop', {
+       prods: products, 
+       pageTitle: 'Shop', 
+       path: '/',
+       hasProducts: products.length > 0,
+       activeShop: true,
+       productCSS: true});
 });
 
 module.exports = router;
