776361dcd995186e82a717822f8c92a6d1f08126
JessD1234 <46542907+JessD1234@users.noreply.github.com>
2021-09-22 19:57:42 -0600

    Group activity

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,6 +3,21 @@
 const express = require('express');
 const router = express.Router();
 
+let users = [];
+
+router.post('/addUser', (req, res, next) => {
+  users.push(req.body.userName);
+  console.log(req.body.userName);
+  res.writeHead(302, {location: '/ta02'});
+  res.end();
+});
+
+router.post('/removeUser', (req, res, next) => {
+  users = users.filter(user => user != req.body.userName);
+  res.write('ta02');
+  res.end();
+});
+
 router.get('/', (req, res, next) => {
   res.render('pages', {
     title: 'Team Activity 02',
@@ -11,5 +26,7 @@ router.get('/', (req, res, next) => {
     contentCSS: true, // For HBS
   });
 });
+
+
 
 module.exports = router;
