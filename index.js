37f43051f76eb5e25240307f6cbac64b2cd1b448
JessD1234 <46542907+JessD1234@users.noreply.github.com>
2021-09-23 15:54:38 -0600

    asdf

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const ta01Routes = require('./routes/ta01');
 const ta02Routes = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03');
 const ta04Routes = require('./routes/ta04');
-const prove02route = require('./prove02/views/shop');
 
 app
   .use(express.static(path.join(__dirname, 'public')))
@@ -39,7 +38,6 @@ app
   .use('/ta02', ta02Routes)
   .use('/ta03', ta03Routes)
   .use('/ta04', ta04Routes)
-  .use('/shop', prove02route)
   .get('/', (req, res, next) => {
     // This is the primary index, always handled last.
     res.render('pages/index', {
