4e0c456d09c45d6de8a4e24384329c9dcce19cca
JessD1234 <46542907+JessD1234@users.noreply.github.com>
2021-11-04 16:35:17 -0600

    Removing duplicate files

--- prove08/routes/prove8-routes.js
+++
@@ -1,8 +0,0 @@
-var jsonEngine = require('../../controllers/prove8-controller');
-
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/', jsonEngine.processJson).post('/', jsonEngine.getIndex);
-module.exports = router;
